1d2d7b66477bdb9e49c4618f3d9c83ebc2adf09c
Charlotta Jung <jungcharlotta@gmail.com>
2017-10-18 20:05:47 +0200

    Add unit tests

---
+++ tests/RestTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martin
+ * Date: 10/18/2017
+ * Time: 7:52 PM
+ */
+
+namespace Maestro\Test;
+
+use Maestro\Rest;
+
+class RestTest extends TestCase
+{
+
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martin
+ * Date: 10/18/2017
+ * Time: 7:57 PM
+ */
+
+namespace Maestro\Test;
+
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
