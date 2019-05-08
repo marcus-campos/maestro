5957e5f44ea9b772fea59db727d5a80db40851a2
Marcus Vinícius Campos <marcus-campos@users.noreply.github.com>
2019-05-08 14:14:12 -0300

    Update Methods.php

--- src/Http/Methods.php
+++ src/Http/Methods.php
@@ -11,154 +11,156 @@ namespace Maestro\Http;
 trait Methods
 {
     private $method;
+    
+    private $endPoint;
 
     /**
      * @return $this
      */
-    public function get()
+    public function get(string $endPoint)
     {
         $this->method = 'GET';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function post()
+    public function post(string $endPoint)
     {
         $this->method = 'POST';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function put()
+    public function put(string $endPoint)
     {
         $this->method = 'PUT';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function delete()
+    public function delete(string $endPoint)
     {
         $this->method = 'DELETE';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function patch()
+    public function patch(string $endPoint)
     {
         $this->method = 'PATCH';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function copy()
+    public function copy(string $endPoint)
     {
         $this->method = 'COPY';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function head()
+    public function head(string $endPoint)
     {
         $this->method = 'HEAD';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function options()
+    public function options(string $endPoint)
     {
         $this->method = 'OPTIONS';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function link()
+    public function link(string $endPoint)
     {
         $this->method = 'LINK';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function unlink()
+    public function unlink(string $endPoint)
     {
         $this->method = 'UNLINK';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function purge()
+    public function purge(string $endPoint)
     {
         $this->method = 'PURGE';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function lock()
+    public function lock(string $endPoint)
     {
         $this->method = 'LOCK';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function unlock()
+    public function unlock(string $endPoint)
     {
         $this->method = 'UNLOCK';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function propfind()
+    public function propfind(string $endPoint)
     {
         $this->method = 'PROPFIND';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 
     /**
      * @return $this
      */
-    public function view()
+    public function view(string $endPoint)
     {
         $this->method = 'VIEW';
-
+        $this->endPoint = $endPoint;
         return $this;
     }
 }
